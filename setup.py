d1c8e97fe562602b855c9eb8507957fcf7f308dc
LearningBoy2002 <bishwarupbbiswas@gmail.com>
2024-09-26 14:32:13 +0530

    all trials completed and template for folder added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medbot',
+    version= '0.0.0',
+    author= 'Bishwarup',
+    packages= find_packages(),
+    install_requires = []
+
+)
